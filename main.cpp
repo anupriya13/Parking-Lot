98a3b366d295c854b510122c20ba3ccfd7a28624
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2021-09-25 09:27:49 +0000

    output in stdout

--- main.cpp
+++ main.cpp
@@ -6,5 +6,16 @@ using namespace std;
 int main()
 {
     Driver d("input.txt");
+    // for input -> input.txt
+    // for output -> output.txt
+
+    // output.txt -> stdout
+    ifstream output;
+    output.open("output.txt");
+    string command;
+    while (getline(output, command)){
+      cout<<command<<endl;
+    }
+
     return 0;
 }
